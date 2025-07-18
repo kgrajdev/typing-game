8cd594c9b7fb815097cd10bf7a25df9abbe3cd88
kgrajdev <kamilgraj@yahoo.com>
2025-07-18 16:53:35 +0100

    config options

--- src/game/main.ts
+++ src/game/main.ts
@@ -1,7 +1,50 @@
-import StartGame from './game/main';
+import { AUTO, Game, Types, Scale } from 'phaser';
+import {PreloadScene} from "./scenes/PreloadScene";
+import {GameOverScene} from "./scenes/GameOverScene";
+import {MainMenuScene} from "./scenes/MainMenuScene";
+import {GameScene} from "./scenes/GameScene";
+import {SettingsScene} from "./scenes/SettingsScene";
+import {AchievementsScene} from "./scenes/AchievementsScene";
+import {HighScoresScene} from "./scenes/HighScoresScene";
+import PauseScene from "./scenes/PauseScene";
 
-document.addEventListener('DOMContentLoaded', () => {
+// Find out more information about the Game Config at:
+// https://docs.phaser.io/api-documentation/typedef/types-core#gameconfig
+const config: Types.Core.GameConfig = {
+    type: AUTO,
+    width: window.innerWidth,
+    height: window.innerHeight,
+    parent: 'game-container',
+    backgroundColor: '#101010',
+    scale: {
+        mode: Scale.FIT,
+        autoCenter: Scale.WIDTH_CONTROLS_HEIGHT
+    },
+    scene: [
+        PreloadScene,
+        MainMenuScene,
+        GameScene,
+        GameOverScene,
+        SettingsScene,
+        AchievementsScene,
+        HighScoresScene,
+        PauseScene
+    ],
+    physics: {
+        default: 'arcade',
+        arcade: {
+            debug: false, // optional
+            gravity: {
+                y: 0,
+                x: 0
+            }
+        }
+    },
+    pixelArt: true
+};
 
-    StartGame('game-container');
+const StartGame = (parent: string) => {
+    return new Game({ ...config, parent });
+}
 
-});
+export default StartGame;
