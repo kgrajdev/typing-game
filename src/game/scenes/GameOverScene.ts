a11bf2f26bacc6a8ca366d0bbeee7d0829d315b1
kgrajdev <kamilgraj@yahoo.com>
2025-07-18 20:47:29 +0100

    game over screen tweaks

--- src/game/scenes/GameOverScene.ts
+++ src/game/scenes/GameOverScene.ts
@@ -18,18 +18,18 @@ export class GameOverScene extends Scene {
         this.currentTheme = this.registry.get('tile-theme')
         const soundManager = SoundManager.getInstance();
         
-        const sceneHeading1 = createImageText(this, "Game", width/2, height/4, checkWordFitmentOnScreen(this, 'game'), this.currentTheme,true, false)
+        const sceneHeading1 = createImageText(this, "Game", width/2, height/6.5, checkWordFitmentOnScreen(this, 'game'), this.currentTheme,true, false)
         createImageText(this, "Over", sceneHeading1.x+96, sceneHeading1.y+64, checkWordFitmentOnScreen(this, 'over'), this.currentTheme,true, false)
 
 
         // score
         createImageText(this, `Score`, width/2, height/2.5, checkWordFitmentOnScreen(this, 'score'), this.currentTheme,true, false)
-        createImageText(this, String(data.score), width/2, height/2.15, checkWordFitmentOnScreen(this, String(data.score)), this.currentTheme,true, false)
+        createImageText(this, String(data.score), width/2, height/2.0, checkWordFitmentOnScreen(this, String(data.score)), this.currentTheme,true, false)
 
         
         // highscore
-        createImageText(this, `HighScore`, width/2, height/1.75, checkWordFitmentOnScreen(this, 'highscore'), this.currentTheme,true, false)
-        createImageText(this, String(data.highScore), width/2, height/1.55, checkWordFitmentOnScreen(this, String(data.highScore)), this.currentTheme,true, false)
+        createImageText(this, `HighScore`, width/2, height/1.50, checkWordFitmentOnScreen(this, 'highscore'), this.currentTheme,true, false)
+        createImageText(this, String(data.highScore), width/2, height/1.30, checkWordFitmentOnScreen(this, String(data.highScore)), this.currentTheme,true, false)
 
 
         createBackButton(this, () => {
